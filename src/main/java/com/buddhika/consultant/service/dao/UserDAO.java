d6aeb7c6b4054223bf438a3505edc62eaaa13e3f
buddhika476 <buddhika476@gmail.com>
2023-09-06 01:46:47 +0530

    Create UserDao crud methods

--- src/main/java/com/buddhika/consultant/service/dao/UserDAO.java
+++ src/main/java/com/buddhika/consultant/service/dao/UserDAO.java
@@ -1,11 +1,16 @@
 package com.buddhika.consultant.service.dao;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import com.buddhika.consultant.service.model.User;
 
 public interface UserDAO {
 	
 	public boolean addUser(User user) throws SQLException, ClassNotFoundException;
+	public boolean editUser(User user) throws SQLException, ClassNotFoundException;
+	public boolean deleteUser(User user) throws SQLException, ClassNotFoundException;
+	public User fetchSingleUser(int appointmentID) throws SQLException, ClassNotFoundException;
+	public List<User> fetchAllUser() throws SQLException, ClassNotFoundException;
 	public boolean loginUser(User user)throws SQLException, ClassNotFoundException;
 }
